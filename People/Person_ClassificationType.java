4937b2e1c0709c14c3d337074ba5eab147eb3f3d
txt112358 <45344051+txt112358@users.noreply.github.com>
2018-11-28 17:15:49 +0800

    Add files via upload

---
+++ People/Person_ClassificationType.java
@@ -0,0 +1,11 @@
+/**
+ * It is used to identify the classiication type of a person list
+ */
+
+/**
+ * @author Tang, Xintian
+ *
+ */
+public enum Person_ClassificationType {
+	SCHOOL, CLASS, INSTITUTE, OTHER;
+}

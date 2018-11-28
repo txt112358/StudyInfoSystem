347a60d3ec264d0e2e7e8bca391cac7aaf3a4768
txt112358 <45344051+txt112358@users.noreply.github.com>
2018-11-28 15:54:33 +0800

    Add files via upload

---
+++ Attributes.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+
+/**
+ * This class is used to record the additional attributes for differet types of person
+ */
+
+/**
+ * @author Tang, Xintian
+ *
+ */
+public class Attributes {
+	HashMap<String, String> attributes = new HashMap<>();
+
+	/**
+	 * Add attribute to the Attributes
+	 * 
+	 * @param key
+	 *            The key field of the attribute
+	 * @param value
+	 *            The value field of the attribute
+	 */
+	public void addAttribute(String key, String value) {
+		assert key != null : "The key cannot be null";
+		assert value != null : "The value cannot be null";
+		this.attributes.put(new String(key), new String(value));
+	}
+
+	/**
+	 * Get the number of attributes
+	 * 
+	 * @return The number of attributes
+	 */
+	public int getAttributeCount() {
+		return this.attributes.size();
+	}
+
+	/**
+	 * Get the attribute based on key
+	 * 
+	 * @param key
+	 *            The key field of the attribute
+	 * @return The String type value
+	 */
+	public String getAttributValue(String key) {
+		assert key != null : "The input key index cannot be null";
+		return new String(this.attributes.get(key));
+	}
+}
